ab74a0e9ba37f9cc66b5e25797a7b4eb0bd00f2a
Fernando <viktorseidl@gmail.com>
2023-11-06 18:17:39 +0100

    homelast

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const lib = require('./src/Utils/connectorHeader');
 const Domaine="https://itsnando.com"
 const PORT = 3000;
 app.use(cors({
-  origin:"*",
+  origin:"https://itsnando.com",
 }))
 /**
  * IMPORT ROUTES
