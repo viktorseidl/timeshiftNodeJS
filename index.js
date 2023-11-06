e1fbaa8fca4a60348ff9a3a3e2430a94e300225b
MKern87 <michael87kern@hotmail.com>
2023-11-06 14:22:03 +0100

    last 06.11.23

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const EncryptionContext=require('./src/routes/EncryptionContext');
  * USE REDIRECT
  */
 app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "itsnando.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   next();
