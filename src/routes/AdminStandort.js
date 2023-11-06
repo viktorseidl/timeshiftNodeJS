b15953fcb6e6b764a6301c8aebcdf4bc4b474dfc
MKern87 <michael87kern@hotmail.com>
2023-11-06 16:20:56 +0100

    last 06.11.23

--- src/routes/AdminStandort.js
+++ src/routes/AdminStandort.js
@@ -7,7 +7,7 @@ const lib = require('../Utils/connectorHeader');
 const Domaine="https://itsnando.com"
 
 router.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "itsnando.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     next();
