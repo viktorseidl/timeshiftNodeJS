e7b1efd570a7a16b547a3edc627ba8f8883c7815
MKern87 <michael87kern@hotmail.com>
2023-11-14 09:50:40 +0100

    everythingworking

--- src/routes/MitarbeiterTimeTracking.js
+++ src/routes/MitarbeiterTimeTracking.js
@@ -8,8 +8,8 @@ const Domaine=lib.getDomain();
 const cors =require('cors'); 
 router.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     next();
   });
 router.use(bodyParser.json({limit: 2500000}))
