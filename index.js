0a491a73f2270d6fa7bf35d6798b820a90f7ae82
MKern87 <michael87kern@hotmail.com>
2023-11-22 12:15:13 +0100

    new Routes

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Methods', 'POST, HEAD, GET, OPTIONS');
-  next();
+  next(); 
 });
 //app.use(bodyParser.json())
-//app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.urlencoded({ extended: false })) 
 
 /**
  * ROUTE HANDLING
