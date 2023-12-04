7e8dd166253e01c88c1838f2987c33ddf1a4465f
MKern87 <michael87kern@hotmail.com>
2023-12-04 12:47:21 +0100

    14515

--- src/routes/TeamPage.js
+++ src/routes/TeamPage.js
@@ -34,7 +34,7 @@ router.use(bodyParser.urlencoded({ extended: false }))
               })            
             };
             const response = await axios.post(
-              `${Domaine}/backend/API/ucontroller/adminpanel/UAdminTeam.php`,
+              `${Domaine}/backend/API/pagecontroll/teampage.php`,
               JSON.stringify({ 
                 T:querytype,
                 StID:stid,
